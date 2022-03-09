1adaf128d1523639d99eedafd043ea60a6efae66
qianli1 <liqian689@hotmail.com>
2022-03-09 14:49:18 -0800

    commit

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -37,7 +37,7 @@ export class ProductListComponent implements OnInit {
   	this.productService.getProducts().subscribe(
         (products: IProduct[]) => {
             this.products = products;
-            this.performFilter(this.listFilter);
+            this.performFilter();
         },
         (error: any) => this.errorMessage = <any>error
     );
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { StarComponent } from './star.component';
+import { CriteriaComponent } from './criteria/criteria.component';
 
 @NgModule({
-  declarations: [StarComponent],
+  declarations: [StarComponent, CriteriaComponent],
   imports: [
     CommonModule,
     FormsModule
   ],
   exports: [
     StarComponent,
+    CriteriaComponent,
     CommonModule,
     FormsModule
   ]
